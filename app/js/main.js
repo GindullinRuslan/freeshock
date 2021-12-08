50b4fb3ab70dd058e16818543792ff45ab70f53d
GindullinRuslan <rusyazlatik@mail.ru>
2021-12-08 21:54:49 +0500

    added lesson 14

--- app/js/main.js
+++ app/js/main.js
@@ -3,9 +3,23 @@ $(function(){
   $('.top-slider__inner').slick({
     prevArrow: '<button type="button" class="top-slider__btn top-slider__btn--prev"><svg class="top-slider__arrow top-slider__arrow--prev"><use xlink:href="./images/sprite.svg#icon-arrow-left"></use></svg></button>',
 
-		nextArrow: '<button type="button" class="top-slider__btn top-slider__btn--next"><svg class="top-slider__arrow top-slider__arrow--next"><use xlink:href="./images/sprite.svg#icon-arrow-right"></use></svg></button>',
+    nextArrow: '<button type="button" class="top-slider__btn top-slider__btn--next"><svg class="top-slider__arrow top-slider__arrow--next"><use xlink:href="./images/sprite.svg#icon-arrow-right"></use></svg></button>',
 
     infinite: false,
+    dots: true,
+  });
+
+
+  $('.burger-btn').on("click",function (event) {
+    $('.media-menu').toggleClass('media-menu--active')
+  });
+
+    $('.media-menu__closebtn').on("click",function (event) {
+    $('.media-menu').removeClass('media-menu--active')
+  });
+
+  $('.user-nav__btn--search').on("click",function (event) {
+    $('.search-form').toggleClass('search-form--active')
   });
 
   $('.header__catalog-btn').on("click",function (event) {
@@ -24,14 +38,25 @@ $(function(){
   var mixer1 = mixitup(containerEl1, config);
   var mixer2 = mixitup(containerEl2, config);
 
+
+  
+
 });
 
 
-$(function(){
-  $('.header__catalog-btn').on('click', function(){
+var inp = document.querySelector('.search-form__input');
+window.addEventListener('resize', changePlaceholder);
+changePlaceholder.call(window);
+
+function changePlaceholder() {
+  inp.setAttribute('placeholder', this.innerWidth >= 1024 ? 'Найти в магазине ...' : 'Я ищу...');
+
+}
+  $(function () {
+    $('.header__catalog-btn').on('click', function () {
       $(this).toggleClass('header__catalog-btn--active');
+    });
   });
-});
 
 
 
